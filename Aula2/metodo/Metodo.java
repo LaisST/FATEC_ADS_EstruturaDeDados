d799e39142daca5f100b6d7b96ed3b9fed6520c9
Lais.ST <74365785+LaisST@users.noreply.github.com>
2022-08-22 10:52:08 -0300

    Update Metodo.java

--- Aula2/metodo/Metodo.java
+++ Aula2/metodo/Metodo.java
@@ -10,12 +10,18 @@ public class Metodo {
 		Calculo calculo1 = new Calculo();
 		
 		Scanner ler = new Scanner (System.in);
-		System.out.println("Digite o n1 ");
-		int n1= ler.nextInt();
-		System.out.println("Digite o n2 ");
-		int n2= ler.nextInt();
+		System.out.println("Digite um número par ");
+		int n1Par= ler.nextInt();
+		System.out.println("Digite outro número par ");
+		int n2Par= ler.nextInt();
+		System.out.println("Digite um número impar ");
+		int n1Impar= ler.nextInt();
+		System.out.println("Digite outro número impar ");
+		int n2Impar= ler.nextInt();
 		
-		System.out.println("O valor maior � " + calculo1.maior(n1,n2));
+		System.out.println("A soma do maior valor par com o maior valor impar é " + calculo1.soma(calculo1.maior(n1Par, n2Par), calculo1.maior(n1Impar, n2Impar)));
+		
+		//System.out.println("O valor maior é " + calculo1.maior(n1,n2));
 		
 		// calculo1.maior(n1,n2);
 		
