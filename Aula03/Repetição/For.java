718d4bfce567667a7226743dd7ad65ab8fbf08f4
Lais.ST <74365785+LaisST@users.noreply.github.com>
2022-08-29 10:55:40 -0300

    Create For.java

---
+++ Aula03/Repetição/For.java
@@ -0,0 +1,22 @@
+package Repeticao;
+/* O FOR necessita de 3 parâmetros:
+1 - A inicialização da variável;
+2 - A condição ou condições que irão manter o looping (repetição);
+3 - O modificador da variável inicialiazada que pode ser incrementada ou decrementada, ou seja, pode aumentar o seu valor ou diminuir
+
+* Os paramentros devem ser separados por ponto e virgula( ; ) 
+* */
+
+public class For {
+
+	public static void main(String[] args) {
+	//for (inicio; condicional ; incremento)
+		for (int i = 0 ; i < 10 ; i++ ) {
+			System.out.println("Bom dia!");
+			System.out.println(i);
+		}
+		
+
+	}
+
+}
