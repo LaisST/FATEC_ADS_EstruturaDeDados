edc4b4223f0e393fdacada060dbed31774f0a207
Lais.ST <74365785+LaisST@users.noreply.github.com>
2022-09-05 16:43:30 -0300

    Update Quadrado.java

--- Lista01/Quadrado.java
+++ Lista01/Quadrado.java
@@ -1,23 +1,46 @@
 import java.util.Scanner;
 
-public class Quadrado {
+public class Quadrado{
 
 	public static void main(String[] args) {
-		int lado;
-		
+	    int lado;
+	    int contador = 0;
+	    int contLado = 0;
+
 		Scanner ler = new Scanner(System.in);
-		System.out.println("Digite o tamanho do lado do quadrado: ");
+		System.out.println("Digite o lado do quadrado: ");
 		lado = ler.nextInt();
-		int c = 1;
 		
-		while (c <= lado) {
-			System.out.print("*");
-			c++;
-			
-		}
-	
+		if (contLado == 0){
+            while (contador < lado){  System.out.print("*"); 
+            contador ++;}
+	    }     
+	   contador = 0;
+	   contLado =1;
+        System.out.println();
+	    
+	    while (contLado < lado-1){
+    	    if (contLado > 0 || contLado < lado -1){
+        	    while (contador < lado){
+        	        if (contador == 0 || contador == lado-1 ) { System.out.print("*");
+        	            contador ++;
+        	            
+        	        }
+        	        else{ System.out.print(" ");
+        	            contador ++;}
+        	    }  
+        	    System.out.println();
+    	    }  
+    	    
+    	   contLado ++;
+    	   contador = 0;
 
+    	   }
+    	       	   
+    	   if (contLado == lado-1 ){
+        	    while (contador < lado){  System.out.print("*"); 
+        	    contador ++;}
+	    } //Fim While
+	} // Fim main
+} // Fim Classe
 
-	}
-	
-}
