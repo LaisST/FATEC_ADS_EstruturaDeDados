a5e608fa70c4b7f7938eb59f1d68c3d8e6f17ba7
Lais.ST <74365785+LaisST@users.noreply.github.com>
2022-09-19 10:11:10 -0300

    Add files via upload

---
+++ Aula05/Main.java
@@ -0,0 +1,17 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		//SomaPares somar1 = new SomaPares();
+		//somar1.SomarPares();
+		
+		//RecebeImparPar recebe1 = new RecebeImparPar();
+		//recebe1.ReceberImparPar();
+		
+		MatrizABC matriz1 = new MatrizABC();
+		matriz1.CalcularABC();
+		
+	}
+
+}
