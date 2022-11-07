d1ff4a4ec5e42ee68bb4791488256d5c634ffbfd
Lais.ST <74365785+LaisST@users.noreply.github.com>
2022-11-07 10:25:41 -0300

    Update InsertSort.java

--- Aula10/src/algoritmoOrdenacao/InsertSort.java
+++ Aula10/src/algoritmoOrdenacao/InsertSort.java
@@ -1,20 +1,37 @@
 package algoritmoOrdenacao;
 
-//Já insere na posição correta
+//JÃ¡ insere na posiÃ§Ã£o correta
 
 public class InsertSort {
-	//Método construtor
+	//MÃ©todo construtor
 	public void InsertSort(int []valores) {
-		for (int i = 1; i<valores.length;i++) { // Indice começa com 1, o laço irá se repetir enquanto o indice for menor do que a quantidade de itens da lista; a cada final de laço irá somar um na variavel i
-			int aux=valores[i]; // variavel auxiliar que irá receber o valor do indice do laço
-			int j=i-1; // a variavel j irá receber o indice menos um, ou seja, a posição anterior
-			while((j>=0)&&(aux<valores[j])) { //Enquanto o valor de j for maior ou igual a zero e o valor do auxiliar for menor do que o valor da posição j o laço irá se repetir
-				valores[j+1] = valores[j]; //a posição de J +1, ou seja, posição atual irá receber o valor da posição anterior
+		for (int i = 1; i<valores.length;i++) { // Indice comeÃ§a com 1, o laÃ§o irÃ¡ se repetir enquanto o indice for menor do que a quantidade de itens da lista; a cada final de laÃ§o irÃ¡ somar um na variavel i
+			int aux=valores[i]; // variavel auxiliar que irÃ¡ receber o valor do indice do laÃ§o
+			int j=i-1; // a variavel j irÃ¡ receber o indice menos um, ou seja, a posiÃ§Ã£o anterior
+			while((j>=0)&&(aux<valores[j])) { //Enquanto o valor de j for maior ou igual a zero e o valor do auxiliar for menor do que o valor da posiÃ§Ã£o j o laÃ§o irÃ¡ se repetir
+				valores[j+1] = valores[j]; //a posiÃ§Ã£o de J +1, ou seja, posiÃ§Ã£o atual irÃ¡ receber o valor da posiÃ§Ã£o anterior
 				j--; // diminui 1 na variavel j
 			}
-			valores [j+1]=aux; // posição j+1 (posição atual) recebe o valor da variavel auxiliar
-		} // continua o laço for até a condição de parada
+			valores [j+1]=aux; // posiÃ§Ã£o j+1 (posiÃ§Ã£o atual) recebe o valor da variavel auxiliar
+		} // continua o laÃ§o for atÃ© a condiÃ§Ã£o de parada
 	}
 	
 
 }
+
+/* Teste de Mesa
+ * 
+ * InserSort(int [5,2,6,7] valores){
+ *  for (i = 1; 1 < 4; 1++){
+ *  	aux = 2;
+ *  	j = 0;
+ *  	while((0>=0)&&(2<5)){
+ *  		valores[1] = 5;
+ *  		-1;
+ *  	}
+ *  	valores[0] = 2;
+ *  }
+ *  } 
+
+ * 
+ * */
